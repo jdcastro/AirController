76ddf488068300b90542ee526500a2ce8200afce
Johnny De Castro <j@jdcastro.co>
2022-09-16 21:18:41 -0500

    Ajustes iniciales: separación de elementos estáticos, reorganización del template para su reuso, uso de la última versión de bulma

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
-from flask import Flask, request, render_template
+from flask import (
+    Blueprint, Flask, flash, g, redirect, render_template, request, session, url_for
+)
+import jinja_partials
 
 app = Flask(__name__)
+
+jinja_partials.register_extensions(app)
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('dashboard/default.html')
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
--- run.sh
+++ run.sh
@@ -2,4 +2,6 @@
 export FLASK_APP=app.py
 export FLASK_DEBUG=1 
 export TEMPLATES_AUTO_RELOAD=1 
+# export SERVER_NAME=airc.jdcastro.co
+# flask run --host=0.0.0.0 --port=8080
 flask run
