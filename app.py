1e7ab8a29212854d406db6d88ddb6d45933edcb6
Johnny De Castro <j@jdcastro.co>
2022-09-21 15:52:49 -0500

    Hito, se cambian los html enviados a formato jinja2 para empezar a usarlos como templates

--- app.py
+++ app.py
@@ -7,14 +7,10 @@ app = Flask(__name__)
 
 jinja_partials.register_extensions(app)
 
-@app.route('/')
-def index():
-    return render_template('dashboard/default.html')
-
 @app.after_request
 def add_header(r):
     """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
+    Add headers to force rendering engine or Chrome Frame,
     and also to cache the rendered page for 10 minutes.
     """
     r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
@@ -22,3 +18,39 @@ def add_header(r):
     r.headers["Expires"] = "0"
     r.headers['Cache-Control'] = 'public, max-age=0'
     return r
+
+@app.route('/')
+def index():
+    return render_template('dashboard/default.html')
+
+@app.route('/vuelos')
+def index():
+    return render_template('dashboard/default.html')
+
+@app.route('/ayuda')
+def index():
+    return render_template('dashboard/default.html')
+
+@app.route('/contacto')
+def index():
+    return render_template('dashboard/default.html')
+
+@app.route('/politicas-privacidad')
+def index():
+    return render_template('dashboard/default.html')
+
+@app.route('/acceso')
+def index():
+    return render_template('dashboard/default.html')
+
+@app.route('/acceso/registro')
+def index():
+    return render_template('dashboard/default.html')
+
+@app.route('/acceso/recuperar-clave')
+def index():
+    return render_template('dashboard/default.html')
+
+@app.route('/dashboard')
+def index():
+    return render_template('dashboard/default.html')
